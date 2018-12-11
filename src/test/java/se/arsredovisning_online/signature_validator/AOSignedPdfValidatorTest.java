c63162b3ce8cb1762485fa94ef496f57ae618298
Mats Sigge <mats.sigge@gmail.com>
2018-12-11 11:51:56 +0100

    Add test cases for audit report and "auditor endorsed" original.

--- src/test/java/se/arsredovisning_online/signature_validator/AOSignedPdfValidatorTest.java
+++ src/test/java/se/arsredovisning_online/signature_validator/AOSignedPdfValidatorTest.java
@@ -15,6 +15,18 @@ public class AOSignedPdfValidatorTest {
     }
 
     @Test
+    public void validatesAuditReport() throws IOException {
+        InputStream pdf = TestUtil.getFixtureFile("/Revisionsberättelse (signerat original) - Demo med revision AB - räkenskapsåret 2017.pdf");
+        assertTrue(new AOSignedPdfValidator(pdf, true).validate());
+    }
+
+    @Test
+    public void validatesAuditorEndorsedReport() throws IOException {
+        InputStream pdf = TestUtil.getFixtureFile("/Årsredovisning (signerat original) - Demo med revision AB - räkenskapsåret 2017.pdf");
+        assertTrue(new AOSignedPdfValidator(pdf, true).validate());
+    }
+
+    @Test
     public void doesNotValidateSealedDocumentWithProdCert() throws IOException {
         InputStream pdf = TestUtil.getFixtureFile("/Årsredovisning (signerat original) - Demobolaget AB - räkenskapsåret 2017.pdf");
         // Use root cert for production to force failure (files are created using test cert)
