74650d98601eb9e9e0e3f5d18762d980f6d8bd3e
Mats Sigge <mats.sigge@gmail.com>
2018-12-11 17:29:51 +0100

    Remove unused import.

--- src/main/java/se/arsredovisning_online/signature_validator/Manifest.java
+++ src/main/java/se/arsredovisning_online/signature_validator/Manifest.java
@@ -5,7 +5,6 @@ import com.google.gson.Gson;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
