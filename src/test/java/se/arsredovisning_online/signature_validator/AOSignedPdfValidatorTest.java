174da5f54088207f5cdd4d39648a887ff54516ce
Mats Sigge <mats.sigge@gmail.com>
2018-12-11 09:28:53 +0100

    Add negative test case.

--- src/test/java/se/arsredovisning_online/signature_validator/AOSignedPdfValidatorTest.java
+++ src/test/java/se/arsredovisning_online/signature_validator/AOSignedPdfValidatorTest.java
@@ -13,4 +13,11 @@ public class AOSignedPdfValidatorTest {
         InputStream pdf = TestUtil.getFixtureFile("/Årsredovisning (signerat original) - Demobolaget AB - räkenskapsåret 2017.pdf");
         assertTrue(new AOSignedPdfValidator(pdf, true).validate());
     }
+
+    @Test
+    public void doesNotValidateSealedDocumentWithProdCert() throws IOException {
+        InputStream pdf = TestUtil.getFixtureFile("/Årsredovisning (signerat original) - Demobolaget AB - räkenskapsåret 2017.pdf");
+        // Use root cert for production to force failure (files are created using test cert)
+        assertFalse(new AOSignedPdfValidator(pdf).validate());
+    }
 }
